4b34b31bae050324cc811fd9ba2756e8370ce942
raworiginal <randall.a.wolfe@gmail.com>
2025-07-04 08:30:38 -0700

    calculate-total-sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,9 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  let totalSale = products.reduce((total, product) => {
+    return total + product.price * product.quantity;
+  }, 0);
+  totalSale += (totalSale * taxRate) / 100;
+  return totalSale;
+}
 
 module.exports = calculateTotalSalesWithTax;
