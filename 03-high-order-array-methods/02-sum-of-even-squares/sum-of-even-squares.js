484b06c4d736735e3ce4d7392bfc6b6b7f564800
raworiginal <randall.a.wolfe@gmail.com>
2025-07-05 07:50:57 -0700

    updated

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evens = numbers.filter((num) => num % 2 === 0);
+  const evenSqrs = evens.map((even) => even ** 2);
+  const sum = evenSqrs.reduce((sum, num) => sum + num, 0);
+  return sum;
+}
 
 module.exports = sumOfEvenSquares;
