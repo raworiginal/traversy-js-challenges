91603499086a75a19b83545ec6e1e14c69b55d98
raworiginal <randall.a.wolfe@gmail.com>
2025-07-05 09:07:02 -0700

    highest scoring word in progress

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,13 @@
-function highestScoringWord() {}
-
+function highestScoringWord(string) {
+  let words = string.split(" ");
+  let scores = words.map((word) => {
+    let score = 0;
+    for (char of word) {
+      score += char.charCodeAt(0) - "a".charCodeAt(0) + 1;
+    }
+    return score;
+  });
+  return scores.reduce((index, score) => {});
+}
+console.log("z".charCodeAt(0) - "a".charCodeAt(0) + 1);
 module.exports = highestScoringWord;
