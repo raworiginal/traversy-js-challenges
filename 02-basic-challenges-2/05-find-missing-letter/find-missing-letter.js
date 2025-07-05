c780f7beabc2a55be2ebfc0f435c8694bc04ddd2
raworiginal <randall.a.wolfe@gmail.com>
2025-07-05 07:48:16 -0700

    updated

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,18 +1,13 @@
 function findMissingLetter(arrayOfChars) {
   const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  console.log(alphabet.length);
   if (!arrayOfChars) return "";
   let offset = alphabet.indexOf(arrayOfChars[0]);
-  console.log(`The Offset is ${offset}`);
   for (let char of arrayOfChars) {
     if (alphabet.indexOf(char) - offset > arrayOfChars.indexOf(char)) {
       missingLetterIdx = arrayOfChars.indexOf(char) + offset;
-      console.log(`MissingLetterIdx = ${missingLetterIdx}`);
       return alphabet[missingLetterIdx];
     }
   }
 }
-console.log(findMissingLetter(["a", "b", "c", "d", "f"])); // => "e"
-console.log(findMissingLetter(["O", "Q", "R", "S"])); // )=> "P"
-console.log(findMissingLetter(["t", "u", "v", "w", "x", "z"])); // => "y"
+
 module.exports = findMissingLetter;
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,12 @@
-function areAllCharactersUnique() {}
-
+function areAllCharactersUnique(string) {
+  let setString = new Set(string.split(""));
+  return Array.from(setString).join("") === string;
+}
+console.log(
+  areAllCharactersUnique("abcdefg"), // true
+  areAllCharactersUnique("abcdefgA"), // true
+  areAllCharactersUnique("programming"), // false
+  areAllCharactersUnique(""), // true
+  areAllCharactersUnique("a") // true
+);
 module.exports = areAllCharactersUnique;
