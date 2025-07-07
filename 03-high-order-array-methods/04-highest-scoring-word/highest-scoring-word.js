6b9a78d809d193edc62792fd900095998a3f62b5
raworiginal <randall.a.wolfe@gmail.com>
2025-07-06 21:58:33 -0700

    hsword

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,13 +1,15 @@
 function highestScoringWord(string) {
-  let words = string.split(" ");
-  let scores = words.map((word) => {
-    let score = 0;
-    for (char of word) {
-      score += char.charCodeAt(0) - "a".charCodeAt(0) + 1;
-    }
-    return score;
+  const words = string.split(" ");
+  const scores = words.map((word) => {
+    Array.from(word).reduce(
+      (score, letter) => score + letter.charCodeAt(0) - 96,
+      0
+    );
   });
-  return scores.reduce((index, score) => {});
+  const highestScore = Math.max(...scores);
+  const highestIndex = scores.indexOf(highestScore);
+  return words[highestIndex];
 }
-console.log("z".charCodeAt(0) - "a".charCodeAt(0) + 1);
+// console.log("z".charCodeAt(0) - "a".charCodeAt(0) + 1);
+console.log(highestScoringWord("man i need a tax up to ubud"));
 module.exports = highestScoringWord;
